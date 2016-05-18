9b1b634a94fa0e74551f2496e73e649d468e741d
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-05-18 20:41:34 +0800

    refactor code to use sum of series to filter out all max profit series

--- src/com/mobkata/GodOfStocks.java
+++ src/com/mobkata/GodOfStocks.java
@@ -4,48 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.mobkata.StockOperation.*;
+import static java.util.stream.Collectors.toList;
 
 public class GodOfStocks {
     public List<List<StockOperation>> operationsForMaxProfit(List<Integer> prices) {
+        List<List<StockOperation>> allStockOperationsSeries = allStockOperationSeries(prices.size());
+
+        int maxProfit = allStockOperationsSeries.stream().
+                mapToInt(series -> sumOf(series, prices)).max().orElse(0);
+
+        return allStockOperationsSeries.stream().
+                filter(series -> sumOf(series, prices) == maxProfit).
+                collect(toList());
+    }
+
+    private List<List<StockOperation>> allStockOperationSeries(int numberOfStockPrices) {
         final List<List<StockOperation>> allStockOperationsSeries = new ArrayList<>();
 
-        if (prices.size() == 0)
+        if (numberOfStockPrices == 0)
             return allStockOperationsSeries;
 
-        if (prices.size() == 1) {
-            allStockOperationsSeries.add(new ArrayList<StockOperation>(){{
-                add(PASS);
-            }});
-        }
-
-        if (prices.size() == 2 && prices.get(0) > prices.get(1))
-            allStockOperationsSeries.add(createStockOperationsSeries(
-                    prices.size() - 1, new ArrayList<StockOperation>() {{
-                        add(PASS);
-                    }}
-            ));
-
-        if (prices.size() == 2 && prices.get(0) == prices.get(1)) {
-            allStockOperationsSeries.add(createStockOperationsSeries(
-                    prices.size() - 1, new ArrayList<StockOperation>() {{
-                        add(BUY);
-                    }}
-            ));
-            allStockOperationsSeries.add(createStockOperationsSeries(
-                    prices.size() - 1, new ArrayList<StockOperation>() {{
-                        add(PASS);
-                    }}
-            ));
-        }
-
-        if (prices.size() == 2 && prices.get(0) < prices.get(1))
-        allStockOperationsSeries.add(createStockOperationsSeries(
-                prices.size() - 1, new ArrayList<StockOperation>() {{
+        allStockOperationsSeries.add(
+                createStockOperationsSeries(numberOfStockPrices - 1, new ArrayList<StockOperation>(){{
+                    add(PASS);
+                }}));
+        allStockOperationsSeries.add(
+                createStockOperationsSeries(numberOfStockPrices - 1, new ArrayList<StockOperation>(){{
                     add(BUY);
-                }}
-        ));
+                }}));
 
         return allStockOperationsSeries;
+    }
+
+    private Integer sumOf(List<StockOperation> series, List<Integer> prices) {
+        int sum = 0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            switch (series.get(i)) {
+                case PASS:
+                    break;
+                case BUY:
+                    sum -= prices.get(i);
+                    break;
+                case SELL:
+                    sum += prices.get(i);
+                    break;
+            }
+        }
+
+        return sum;
     }
 
     private List<StockOperation> createStockOperationsSeries(int numberOfPrices, List<StockOperation> currentSeries) {
