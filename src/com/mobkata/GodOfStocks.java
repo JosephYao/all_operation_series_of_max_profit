00318fcb5791159a73632e489259b96464d8de13
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-05-18 18:27:07 +0800

    refactoring the code to the recursive style as what I believe the algorithm will be

--- src/com/mobkata/GodOfStocks.java
+++ src/com/mobkata/GodOfStocks.java
@@ -4,21 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.mobkata.StockOperation.*;
-import static java.util.Arrays.asList;
 
 public class GodOfStocks {
     public List<List<StockOperation>> operationsForMaxProfit(List<Integer> prices) {
         final List<List<StockOperation>> allStockOperationsSeries = new ArrayList<>();
 
-        if (prices.size() == 1)
-            allStockOperationsSeries.add(asList(PASS));
+        if (prices.size() == 0)
+            return allStockOperationsSeries;
 
-        if (prices.size() == 2) {
-            allStockOperationsSeries.add(asList(PASS, PASS));
-            if (prices.get(0) == prices.get(1))
-                allStockOperationsSeries.add(asList(BUY, SELL));
-        }
+        allStockOperationsSeries.add(createStockOperationsSeries(
+                prices.size() - 1, new ArrayList<StockOperation>() {{
+                    add(PASS);
+                }}
+        ));
+        if (prices.size() == 2 && prices.get(0) == prices.get(1))
+            allStockOperationsSeries.add(createStockOperationsSeries(
+                    prices.size() - 1, new ArrayList<StockOperation>() {{
+                        add(BUY);
+                    }}
+            ));
 
         return allStockOperationsSeries;
+    }
+
+    private List<StockOperation> createStockOperationsSeries(int numberOfPrices, List<StockOperation> currentSeries) {
+        if (numberOfPrices == 0)
+            return currentSeries;
+
+        if (lastOperationOf(currentSeries) == PASS)
+            currentSeries.add(PASS);
+        else
+            currentSeries.add(SELL);
+
+        return currentSeries;
+    }
+
+    private StockOperation lastOperationOf(List<StockOperation> currentSeries) {
+        return currentSeries.get(currentSeries.size() - 1);
     }
 }
