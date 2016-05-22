fbad51e8c59e6504ddc38b24e74e5f9c23a1ff67
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-05-22 15:44:13 +0800

    pass all acceptance tests from Sun

--- test/com/mobkata/AcceptanceTestFromSun.java
+++ test/com/mobkata/AcceptanceTestFromSun.java
@@ -28,6 +28,20 @@ public class AcceptanceTestFromSun {
         assertStockOperationEquals(asList(asList(BUY, PASS, PASS, PASS, SELL, COOL)), prices);
     }
 
+    @Test
+    public void select_1_from_2() {
+        List<Integer> prices = asList(1,4,2,3,2,1);
+        assertEquals(3, godOfStocks.maxProfit(prices));
+        assertStockOperationEquals(asList(asList(BUY, SELL, COOL, PASS, PASS, PASS)), prices);
+    }
+    
+    @Test
+    public void sell_ealier_for_next_raise() {
+        List<Integer> prices = asList(1,3,4,2,5,2);
+        assertEquals(5, godOfStocks.maxProfit(prices));
+        assertStockOperationEquals(asList(asList(BUY, SELL, COOL, BUY, SELL, COOL)), prices);
+    }
+
     private void assertStockOperationEquals(List<List<StockOperation>> expected, List<Integer> prices) {
         List<List<StockOperation>> actual = godOfStocks.operationsForMaxProfit(prices);
         assertEquals(expected.size(), actual.size());
