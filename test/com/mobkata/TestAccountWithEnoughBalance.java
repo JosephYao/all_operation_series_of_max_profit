79a94036a32a20204b6e905336516d766a38d648
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-06-03 22:21:19 +0800

    refactor: move test to the right place

--- test/com/mobkata/TestAccountWithEnoughBalance.java
+++ test/com/mobkata/TestAccountWithEnoughBalance.java
@@ -2,8 +2,14 @@ package com.mobkata;
 
 import org.junit.Test;
 
-import static com.mobkata.TestAccountHelper.*;
+import java.util.function.Consumer;
+
+import static com.mobkata.TestAccountHelper.ENOUGH_BALANCE;
+import static com.mobkata.TestAccountHelper.PRICE;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class TestAccountWithEnoughBalance {
 
@@ -29,4 +35,14 @@ public class TestAccountWithEnoughBalance {
 
         assertEquals(1, account.profit());
     }
+
+    @Test
+    public void buy_with_enough_balance_should_call_the_consumer() {
+        Consumer mockConsumer = mock(Consumer.class);
+
+        account.buy(PRICE, mockConsumer);
+
+        verify(mockConsumer).accept(any(Account.class));
+    }
+
 }
